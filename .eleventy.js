1bababf4705556d11d5c676289049e7895916546
Greg Opperman <greg@patientdiscovery.com>
2025-08-17 18:15:21 -0400

    Total donations table

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,25 @@
 module.exports = function (eleventyConfig) {
   // Passthroughs
-  eleventyConfig.addPassthroughCopy("style.css");
-  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("style.css")
+  eleventyConfig.addPassthroughCopy("images")
 
   //Config
-  eleventyConfig.setOutputDirectory("docs");
-  eleventyConfig.setLayoutsDirectory("_layouts");
-  eleventyConfig.setTemplateFormats("html,liquid");
+  eleventyConfig.setOutputDirectory("docs")
+  eleventyConfig.setLayoutsDirectory("_layouts")
+  eleventyConfig.setTemplateFormats("html,liquid")
   eleventyConfig.addGlobalData('layout', 'default.liquid')
+
+  // Collections
+  eleventyConfig.addCollection("candidates", (collectionApi) => {
+    return collectionApi.getFilteredByGlob("./candidates/*.liquid")
+  })
+
+  // Filters
+  eleventyConfig.addFilter("district", (v) => {
+    if (v[0] && v[0] === 'atlarge') {
+      return v[0].replace("atlarge", "At-Large")
+    } else {
+      return v[0] && v[0].toUppercase
+    }
+  })
 }
